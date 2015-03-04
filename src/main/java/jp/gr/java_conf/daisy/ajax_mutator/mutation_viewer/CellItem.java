7877224c9a9a76edf39b18369308c3de214559e6
Yuta Maezawa <maezawa@nii.ac.jp>
2015-03-04 16:23:36 +0000

    Maven artifacts for 0.0.2-SNAPSHOT

--- src/main/java/jp/gr/java_conf/daisy/ajax_mutator/mutation_viewer/CellItem.java
+++
@@ -1,5 +0,0 @@
-package jp.gr.java_conf.daisy.ajax_mutator.mutation_viewer;
-
-public interface CellItem {
-    public String getDisplayName();
-}
--- src/test/java/jp/gr/java_conf/daisy/ajax_mutator/detector/request_detector/RequestDetectorTest.java
+++
@@ -1,5 +0,0 @@
-package jp.gr.java_conf.daisy.ajax_mutator.detector.request_detector;
-
-public class RequestDetectorTest {
-
-}
